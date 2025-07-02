79e2ce3aa08b98563b599094740ae4b174edfc01
Wiwin Wibawa <wiwinwibawa27@gmail.com>
2025-07-02 15:28:44 +0800

    Create frontend and add configuration

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from './config/db.js';
 
 import productRoutes from "./routes/product.route.js";
@@ -7,13 +9,24 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // allow to access json in request.body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+
+app.listen(PORT, () => {
     connectDB();
-    console.log("server strted at http://localhost:5000")
+    console.log("server strted at http://localhost:" + PORT)
 });
 
